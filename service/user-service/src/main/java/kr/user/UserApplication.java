1c42cb6307d54820e02891532e6d69286464a760
zeongii <yaksanjy@naver.com>
2024-10-17 13:15:39 +0900

    eureka

--- service/user-service/src/main/java/kr/user/UserApplication.java
+++ service/user-service/src/main/java/kr/user/UserApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@EnableDiscoveryClient
 @EnableReactiveMongoRepositories
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
