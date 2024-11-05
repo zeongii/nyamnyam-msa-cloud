4757d670967d45d44932eb81691124341b6e1c67
zeongii <yaksanjy@naver.com>
2024-11-05 18:24:47 +0900

    deploy

--- server/gateway-server/src/main/java/kr/gateway/config/WebSecurityConfig.java
+++ server/gateway-server/src/main/java/kr/gateway/config/WebSecurityConfig.java
@@ -58,7 +58,10 @@ public class WebSecurityConfig {
                 "http://localhost:3000",
                 "https://www.nyamnyam.kr",
                 "https://abc.nyamnyam.kr",
-                "https://jiyeong.nyamnyam.kr"
+                "https://jiyeong.nyamnyam.kr",
+                "https://nyamnyam-vercel-front-78ywlgrc9-zeongiis-projects.vercel.app/",
+                "https://nyamnyam-vercel-front-git-main-zeongiis-projects.vercel.app/"
+
         ));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
